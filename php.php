42d8f7e835dd853a7b26929e3983dd4a5e45b037
Sajjad4297 <sajjad.ranjbaran.4297@gmail.com>
2021-12-28 19:50:54 +0330

    s

--- php.php
+++ php.php
@@ -1,7 +1,2 @@
 <?php
-$path='upload/';
-$path.=$_FILES['image']['name'];
-if (! is_dir('upload')){
-    mkdir('upload');
-}
-move_uploaded_file($_FILES['image']['tmp_name'] , $path);
+echo 'sajjad';
